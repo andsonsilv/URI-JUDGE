517ece2109ce15659c041dd7198fadd199280883
Antônio Andson da Silva <andsonsilva@alu.ufc.br>
2018-01-11 00:03:09 -0300

    Diferença

---
+++ 1007.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+
+    int a, b, c, d, diferenca;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    diferenca = a*b - c*d;
+    printf("DIFERENCA = %d\n", diferenca);
+    
+    return 0;
+}
