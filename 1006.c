0c69088f639d13a825ac04dc6aa0831ddd5a0b63
Antônio Andson da Silva <andsonsilva@alu.ufc.br>
2018-01-11 00:02:02 -0300

    Média 2

---
+++ 1006.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    float a, b, c, media;
+    scanf("%f %f %f", &a,&b,&c);
+    media = ((a*2)+ (b*3) + (c*5))/ 10;
+    printf("MEDIA = %.1f\n", media);
+
+	return 0;
+}
