303151a493af68f1abb0add6f411f5316fd8eabc
Antônio Andson da Silva <andsonsilva@alu.ufc.br>
2018-01-11 14:11:44 -0300

    Múltiplos

---
+++ 1044.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    if (a >= b && a % b == 0){
+        printf("Sao Multiplos\n");
+
+    } else if (b >= a && b % a == 0){
+        printf("Sao Multiplos\n");
+    } else {
+        printf("Nao sao Multiplos\n");
+    }
+
+    return 0;
+}
