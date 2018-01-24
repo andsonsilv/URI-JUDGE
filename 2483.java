48bb910076063c03ba2b7c64d4467db95a5fcc3f
Antônio Andson da Silva <andsonsilva@alu.ufc.br>
2018-01-23 22:06:22 -0300

    Feliz nataaal!

---
+++ 2483.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner entrada = new Scanner(System.in);
+        int x;
+        x = entrada.nextInt();
+        System.out.printf("Feliz nat");
+        while(x > 0){
+        	System.out.printf("a");
+        	x--;
+        }
+        System.out.printf("l!\n");
+    }
+    
+}
