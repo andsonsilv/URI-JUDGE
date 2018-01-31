dcab90abf09785740ededa1f665d6deb3e4ff1e0
Antônio Andson da Silva <andsonsilva@alu.ufc.br>
2018-01-31 00:56:57 -0300

    Ho Ho Ho

---
+++ 1759.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(void){
+ 
+    int a;
+    cin >> a;
+    for(int i = 0; i < a - 1; i++){
+        cout << "Ho" << " ";
+    }
+    cout << "Ho!" << endl;
+ 
+ 
+    return 0;
+}
