421edf8203243314149733aca665f2a2d749d3a2
Antônio Andson da Silva <andsonsilva@alu.ufc.br>
2018-01-31 00:52:11 -0300

    Acima da Diagonal Principal

---
+++ 1183.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+int main(void){
+    char x;
+    float matriz[12][12];
+    cin >> x;
+    int haha = 0;
+    float media = 0, soma = 0;
+
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            cin >> matriz[i][j];
+        }
+    }
+    if(x == 'S'){
+        for(int i = 0; i < 12; i++){
+            for(int j = 11; j > i; j--){
+                haha++;
+                soma += matriz[i][j];
+            }
+        }
+        printf("%0.1f\n",soma );
+    }else if(x == 'M'){
+        for(int i = 0; i < 12; i++){
+            for(int j = 11; j > i; j--){
+                haha++;
+                soma += matriz[i][j];
+            }
+        }
+        media = soma / (haha * 1.0);
+        printf("%0.1f\n",media );
+    }
+    return 0;
+}
