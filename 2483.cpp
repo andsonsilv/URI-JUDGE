fa445f7c6604add81e1d4b0a6be728a4900d87f6
Antônio Andson da Silva <andsonsilva@alu.ufc.br>
2018-01-31 01:35:19 -0300

    Feliz Nataaal!

---
+++ 2483.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int x;
+    cin >> x;
+    cout << "Feliz nat";
+    while(x > 0){
+        cout << "a";
+        x--;
+    }
+    cout << "l!\n";
+    return 0;
+}
